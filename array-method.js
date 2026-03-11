1d269bcc87557797fb6b8c08fe9fb4fe1d88dc4b
niloy <niloypaul81@gmail.com>
2026-03-11 14:00:35 +0600

    array method map

--- array-method.js
+++ array-method.js
@@ -1,1 +1,29 @@
 //map,filter,find,reduce
+
+const products = [
+  {
+    id: 1,
+    name: "Laptop",
+    price: 800,
+    brand: "Dell"
+  },
+  {
+    id: 2,
+    name: "Mobile",
+    price: 500,
+    brand: "Samsung"
+  },
+  {
+    id: 3,
+    name: "Headphone",
+    price: 100,
+    brand: "Sony"
+  }
+];
+
+const productLIst= products.map((pd)=>pd.price>100 ?{
+    ...pd,isExpensive: true}:{...pd,isExpensive: false}
+
+);
+
+console.log(productLIst);
